fe0475743d420cb16866dee836fb6dd1589f7502
Brian Andhika Putra <135279979+brianandhikap@users.noreply.github.com>
2025-06-01 09:16:10 +0700

    pppoe_secret.php

---
+++ pppoe_secret.php
@@ -0,0 +1,25 @@
+<?php
+require('routeros_api.class.php');
+header('Content-Type: application/json');
+
+$API = new RouterosAPI();
+$host = '0.0.0.0'; // IP Mikrotik
+$user = 'username'; // Username
+$pass = 'password'; // Password
+
+if ($API->connect($host, $user, $pass)) {
+    $API->write('/ppp/secret/print');
+    $read = $API->read();
+    $API->disconnect();
+
+    echo json_encode([
+        'status' => 'success',
+        'data' => $read
+    ]);
+} else {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Unable to connect to MikroTik API'
+    ]);
+}
+?>
